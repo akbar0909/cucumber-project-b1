50089aaf5e926e3d33954740982b7584d36fd0a7
Akbar <you@example.com>
2023-06-16 16:40:30 -0400

    CukesRunner

---
+++ src/test/java/com/loop/utilities/DocuportConstants.java
@@ -0,0 +1,59 @@
+package com.loop.utilities;
+
+public class DocuportConstants {
+
+    public static final String USERNAME_CLIENT = "b1g1_client@gmail.com";
+    public static final String USERNAME_ADVISER = "b1g1_advisor@gmail.com";
+    public static final String PASSWORD_CLIENT = "Group1";
+    public static final String PASSWORD_ADVISOR = "Group1";
+    public static final String ERROR_MESSAGE_FOR_EMPTY_PASSWORD = "Please enter your password";
+    public static final String LOGO_DOCUPORT = "Docuport";
+
+    public static final String ADRESS_DOCUPORT = "https://beta.docuport.app/";
+
+
+    public static final String EXPECTED_PLACEHOLDER_USER_NAME = "Username or Email";
+    public static final String EXPECTED_PLACEHOLDER_PASSWORD = "Password";
+    public static final String RESET_PASSWORD_URL = "reset-password";
+
+    public static final String RESET_PASSWORD_MESSAGE = "Enter the email address associated with your account";
+
+    public static final String EMAIL_FOR_RESET_PASSWORD = "forgotpasswordg1@gmail.com";
+    public static final String USERNAME_ADVISOR = "b1g1_advisor@gmail.com";
+    public static final CharSequence USERNAME_SUPERVISOR = "b1g1_supervisor@gmail.com";
+    public static final CharSequence USERNAME_EMPLOYEE ="b1g1_employee@gmail.com";
+    public static final CharSequence PASSWORD = "Group1";
+
+    public static final String ADVISOR = "advisor";
+    public static final String CLIENT = "client";
+    public static final String SUPERVISOR = "supervisor";
+    public static final String EMPLOYEE = "employee";
+    public static final String ALEX_ADDRESS = "alex.de.souza@gmail.com";
+    public static final String ACTUAL_FULL_NAME = "Full name";
+    public static final String EXPECTED = "Alex De Souza";
+    public static final String USERS = "//span[contains(text(),'Users')]";
+    public static final String ACTUAL_USName= "username";
+    public static final String EXPECTED_USName= "alexdesouze";
+    public static final String ACTUAL_PHONE_NUM= "phone number";
+    public static final String EXPECTED_ALEX_PH_NUM = "+994512060042";
+    public static final String  EXPECTED_CLIENT = "Client";
+    public static final String  ACTUAL_ROLE = "Role";
+    public static final String  ACTUAL_ADVISOR = "advisor";
+    public static final String  EXPECTED_GROUP = "Batch1 Group1";
+    public static final String SUPER_ADDRESS = "b1g1_supervisor@gmail.com";
+    public static final String EXPECTED_PHONE_NUM = "0000000000";
+    public static final String EXPECTED_SUP_ROLE = "Supervisor";
+    public static final String EXPECTED_EMPTY = "";
+     public static final String ADVISOR_EMAIL = "b1g1_advisor@gmail.com";
+
+    public static final String EXPECTED_ADVISOR_ROLE = "Advisor";
+
+
+
+
+
+
+
+    // "label[for='input-14']";  label[for='input-15']
+
+}
---
+++ src/test/resources/features/loginFeature.feature
@@ -0,0 +1,16 @@
+Feature: Docuport Login Logout Feature
+
+  Scenario: Login as a client
+    Given  user is on Docuport login page
+    When user enters username for client
+    And user enters password for client
+    And use clicks login button
+    Then user should see the home page for client
+
+  Scenario: Login as a employee
+    Given  user is on Docuport login page
+    When user enters username for employee
+    And user enters password for employee
+    And use clicks login button
+    Then user should see the home page for employee
+
