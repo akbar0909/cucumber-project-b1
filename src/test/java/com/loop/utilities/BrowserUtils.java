9d9a6cde5780fa34bbffa5a9176ccd042ea3d0c6
Akbar <you@example.com>
2023-06-19 11:18:18 -0400

    CukesRunner

---
+++ src/test/java/com/loop/utilities/BrowserUtils.java
@@ -0,0 +1,187 @@
+package com.loop.utilities;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.Set;
+
+import static org.testng.Assert.assertTrue;
+
+public class BrowserUtils {
+
+
+    /**
+     * validate if driver switched to expected ur and title
+     *
+     * @param driver
+     * @param expectedUrl
+     * @param expectedTitle
+     * @author akbar
+     * implements assertion
+     * note: usually we do not do validation in method, this is for practicing purposes
+     */
+    public static void switchWindowAndValidate(WebDriver driver, String expectedUrl, String expectedTitle) {
+        expectedUrl = expectedUrl.toLowerCase();
+        expectedTitle = expectedTitle.toLowerCase();
+        Set<String> windowHandles = driver.getWindowHandles();
+        for (String each : windowHandles) {
+            driver.switchTo().window(each);
+            if (driver.getCurrentUrl().toLowerCase().contains(expectedUrl)) {
+                break;
+            }
+        }
+        assertTrue(driver.getTitle().toLowerCase().contains(expectedTitle));
+    }
+
+
+    /**
+     * switches to new window by the exact title
+     * returns to original window if windows with given title not found
+     */
+    public static void switchToWindow(WebDriver driver, String targetTitle) {
+        String origin = driver.getWindowHandle();
+        for (String handle : driver.getWindowHandles()) {
+            driver.switchTo().window(handle);
+            if (driver.getTitle().contains(targetTitle)) {
+                return;
+            }
+        }
+        driver.switchTo().window(origin);
+    }
+
+    /**
+     * @param driver
+     * @param expectedTitle returns void, assertion is implemented
+     * @authot akbar
+     */
+    public static void validateTitle(WebDriver driver, String expectedTitle) {
+        assertTrue(driver.getTitle().contains(expectedTitle));
+    }
+
+    /**
+     * @param nameOfthePage from loop practice page
+     *                      once you on the page call it
+     * @aothor akber
+     */
+    public static void loopLinkClick(String nameOfthePage) {
+        WebElement element = Driver.getDriver().findElement(By.xpath("//a[.='" + nameOfthePage + "']"));
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.elementToBeClickable(element));
+        element.click();
+    }
+
+    /**
+     * Moves the mouse to given element
+     *
+     * @param element on which to hover
+     * @author akbar
+     */
+    public static void hover(WebElement element) {
+        Actions actions = new Actions(Driver.getDriver());
+        actions.moveToElement(element).perform();
+    }
+
+    /**
+     * Scrolls down to an element using JavaScript
+     *
+     * @param element
+     * @author akbar
+     */
+    public static void scrollToElement(WebElement element) {
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
+    }
+
+    /**
+     * Clicks on an element using JavaScript
+     *
+     * @param element
+     * @author akbar
+     */
+    public static void clickWithJS(WebElement element) {
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].scrollIntoView(true);", element);
+        ((JavascriptExecutor) Driver.getDriver()).executeScript("arguments[0].click();", element);
+    }
+
+    /**
+     * Performs double click action on an element
+     *
+     * @param element
+     * @author akbar
+     */
+    public static void doubleClick(WebElement element) {
+        new Actions(Driver.getDriver()).doubleClick(element).build().perform();
+    }
+
+    /**
+     * Waits for the provided element to be visible on the page
+     *
+     * @param element
+     * @param timeToWaitInSec
+     * @return
+     * @author akbar
+     */
+    public static WebElement waitForVisibility(WebElement element, int timeToWaitInSec) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(timeToWaitInSec));
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    /**
+     * Waits for the provided element to be invisible on the page
+     *
+     * @param element
+     * @param timeToWaitInSec
+     * @return
+     * @author akbar
+     */
+    public static void waitForInVisibility(WebElement element, int timeToWaitInSec) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(timeToWaitInSec));
+        wait.until(ExpectedConditions.invisibilityOf(element));
+    }
+
+    /**
+     * Waits for provided element to be clickable
+     *
+     * @param element
+     * @param timeout
+     * @return
+     * @author akbar
+     */
+    public static WebElement waitForClickable(WebElement element, int timeout) {
+        WebDriverWait wait = new WebDriverWait(Driver.getDriver(), Duration.ofSeconds(timeout));
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    /**
+     * performs a pause
+     *
+     * @param seconds
+     * @author nsh
+     */
+    public static void justWait(int seconds) {
+        try {
+            Thread.sleep(seconds * 1000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void selectCheckBox(WebElement element, boolean check) {
+        if (check) {
+            if (!element.isSelected()) {
+                element.click();
+            }
+        } else {
+            if (!element.isSelected()) {
+                element.click();
+            }
+        }
+    }
+}
+
+
